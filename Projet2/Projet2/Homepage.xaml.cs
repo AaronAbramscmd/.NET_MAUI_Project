f279679e08cb6677ebd57c08cf16650fdcb4048f
AaronAbramscmd <abrams433@morrisville.edu>
2025-05-03 02:20:31 -0400

    Form functionality

--- Projet2/Projet2/Homepage.xaml.cs
+++ Projet2/Projet2/Homepage.xaml.cs
@@ -10,6 +10,20 @@ public partial class Homepage : ContentPage
 	}
     private void OnClickExam(object sender, EventArgs e)
 	{
-		Shell.Current.GoToAsync("ExamPage");
+        string StudentID = ID.Text;
+        string FirstN = Fname.Text;
+        string LastN = Lname.Text;
+        string Gmail = Email.Text;
+
+        if (string.IsNullOrEmpty(StudentID) || string.IsNullOrEmpty(FirstN) ||
+            string.IsNullOrEmpty(LastN) || string.IsNullOrEmpty(Gmail))
+        {
+           FailedForm.Text = "All fields must be filled"; 
+        }
+        else
+        {
+            Shell.Current.GoToAsync("ExamPage");
+        }
+        
 	}
 }
